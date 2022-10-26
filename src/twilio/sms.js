784eb1a463c08727e8200e12c6f6971eae616f44
francocornejo <franco-cornejo@hotmail.com>
2022-10-26 20:28:40 -0300

    graphQL

--- src/twilio/sms.js
+++ src/twilio/sms.js
@@ -5,17 +5,16 @@ const authToken = "609defe7b5cc48028a591636e7273a00"//process.env.ACCOUNT_TOKEN
 
 const client = twilio(accountSid, authToken)
 
-export default async function envioSms(to,body) {
+export default async function mainWhatsapp(body) {
     await client.messages.create({
-        body: body,
-        from: '+19592711872',
-        to: `+54${to}`
-      },(err, info)=>{
-          if (err) {
-              console.log("Este Error es de mensajes: ", err)
-          } else {
-              console.log('sms sent: ', info);
-          }
-      })
-  }
-
+       body: body,
+       from: 'whatsapp:+14155238886',
+       to: "whatsapp:+5491140330618"
+     },(err, info)=>{
+         if (err) {
+             console.log("Este Error es de whatsapp: ", err)
+         } else {
+             console.log('whatsapp sent: ', info);
+         }
+     })
+ }
